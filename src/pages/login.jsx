a3eb5f707ec4a086f3bee062babe611fa0bd5f09
Denis Arger <den.arger@gmail.com>
2023-12-05 15:32:59 +0300

    feat: add input password

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -11,11 +11,21 @@ export default function Login() {
   const supabase = useSupabaseClient();
   const router = useRouter();
   const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
   const [error, setError] = useState(false);
-
+  const [showPasswordInput, setShowPasswordInput] = useState(false);
+  const [isLoginButtonActive, setIsLoginButtonActive] = useState(true);
   const handleLogout = async () => {
     try {
-      const { error } = await supabase.auth.signOut();
+      // const { error } = await supabase.auth.signOut();
+
+      if (!isLoginButtonActive) {
+        setShowPasswordInput(false);
+        setIsLoginButtonActive(true);
+      } else {
+        router.push(router.query?.redirectedFrom ?? "/projects");
+      }
+
       if (error) throw error;
       setError(false);
       router.push("/login");
@@ -26,12 +36,17 @@ export default function Login() {
 
   const handleLogin = async () => {
     try {
+      if (isLoginButtonActive) {
+        setShowPasswordInput(true);
+        setIsLoginButtonActive(false);
+      } else {
+        router.push(router.query?.redirectedFrom ?? "/projects");
+      }
       // const { error } = await supabase.auth.signIn({
       //   email,
       // });
       // if (error) throw error;
       setError(false);
-      router.push(router.query?.redirectedFrom ?? "/projects");
     } catch (error) {
       setError(error.message);
     }
@@ -80,7 +95,6 @@ export default function Login() {
               >
                 Ru
               </div>
-
               <div
                 className={`ml-2 mt-4 cursor-pointer text-sm ${
                   i18n.language === "en" ? "text-gray-500" : ""
@@ -104,18 +118,40 @@ export default function Login() {
                   className="block w-full rounded-none border-b border-gray-300 px-2 py-1.5 text-gray-900 shadow-none focus:ring-0 focus:border-0 focus:border-b-2 focus:border-primary focus:outline-none focus:ring-transparent sm:text-sm sm:leading-6"
                 />
               </div>
+              {showPasswordInput && (
+                <div>
+                  <label className="block text-sm font-medium leading-6 text-gray-900">
+                    {t("Password")}
+                  </label>
+                  <input
+                    type="password"
+                    placeholder="password"
+                    value={password}
+                    onChange={({ target: { value } }) => setPassword(value)}
+                    className="block w-full rounded-none border-b border-gray-300 px-2 py-1.5 text-gray-900 shadow-none focus:ring-0 focus:border-0 focus:border-b-2 focus:border-primary focus:outline-none focus:ring-transparent sm:text-sm sm:leading-6"
+                  />
+                </div>
+              )}
             </form>
             <br />
             <p>{error}</p>
             <div className="flex">
               <button
-                className="flex w-full justify-center rounded-md bg-primary px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-                onClick={handleLogin}
+                className={`flex w-full justify-center rounded-md ${
+                  isLoginButtonActive
+                    ? "bg-primary text-white hover:bg-zinc-700"
+                    : "bg-zinc-200 text-black hover:bg-zinc-300 "
+                } px-3 py-1.5 text-sm font-semibold leading-6 shadow-sm focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600`}
+                onClick={handleLogout}
               >
                 {t("SignIn")}
               </button>
               <button
-                className="flex w-full justify-center rounded-md px-3 py-1.5 text-sm font-semibold leading-6 text-black shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                className={`flex w-full justify-center rounded-md ${
+                  isLoginButtonActive
+                    ? "bg-zinc-200 text-black hover:bg-zinc-300 "
+                    : "bg-primary text-white hover:bg-zinc-700 "
+                } px-3 py-1.5 text-sm font-semibold leading-6 text-black shadow-sm focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600`}
                 onClick={handleLogin}
               >
                 {t("Login")}
